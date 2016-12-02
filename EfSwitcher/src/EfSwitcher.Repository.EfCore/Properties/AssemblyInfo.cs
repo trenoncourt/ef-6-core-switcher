e79f3aa2a46af4f534217c1782d4f1bf29e23d3c
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2016-12-02 08:19:39 +0100

    Add Repository projects

---
+++ EfSwitcher/src/EfSwitcher.Repository.EfCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EfSwitcher.Repository.EfCore")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a510c9ca-b4c2-4c8a-a78f-08ca3ecf3bc3")]
